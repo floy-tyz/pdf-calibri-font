166807a6a17dc686b4c79a7fd6347be5f85e0e79
floy-dev <floy-dev@mail.ru>
2024-11-03 16:19:56 +0300

    make

--- make.sh
+++ make.sh
@@ -44,7 +44,7 @@ cd "$tmp_dir"
 err=0
 
 echo -e "\n:: Downloading PowerPoint Viewer...\n"
-wget -O "$file" https://web.archive.org/web/20171225132744/http://download.microsoft.com/download/E/6/7/E675FFFC-2A6D-4AB0-B3EB-27C9F8C8F696/PowerPointViewer.exe
+wget -O "$file" https://github.com/floy-tyz/pdf-calibri-font/raw/refs/heads/main/PowerPointViewer.exe
 if [ $? -ne 0 ]; then
     rm -f "$file"
     echo -e "\nError: Download failed!?\n"
